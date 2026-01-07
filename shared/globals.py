d83992a9a72e6c71a16d73200979329f36c8eb95
hytheaway <91982175+hytheaway@users.noreply.github.com>
2026-01-07 17:30:37 -0500

    Add global variables space

---
+++ shared/globals.py
@@ -0,0 +1,18 @@
+# Global variables are stored here. This file should not import any other.
+
+root = None
+icon_photo = None
+
+audio_file_path = None
+audio_file_path_print = None
+audio_file_path_short = None
+
+IR = None
+fs = None
+
+graph_buttons_state = 'disabled'
+file_data_button = None
+td_vis_button = None
+fd_vis_button = None
+
+file_data_window = None
