0ae826c5c2a749f64fbe84737ac9db50537b9dbe
hytheaway <91982175+hytheaway@users.noreply.github.com>
2026-01-07 17:28:32 -0500

    Add mixer init and snake case func names

--- gui/soundio.py
+++ gui/soundio.py
@@ -3,7 +3,10 @@
 import soundfile as sf
 import pygame
 
-def playAudio(path_to_audio_file: str):
+pygame.mixer.pre_init(channels=1, allowedchanges=pygame.AUDIO_ALLOW_CHANNELS_CHANGE)
+pygame.init()
+
+def play_audio(path_to_audio_file: str):
     """
     Uses pygame to play audio in the app.
 
@@ -14,7 +17,10 @@ def playAudio(path_to_audio_file: str):
     pygame.mixer.music.load(path_to_audio_file)
     pygame.mixer.music.play()
 
-def stopAudioAndCloseWindow(window_to_close):
+def pause_audio():
+    pygame.mixer.music.pause()
+
+def stop_audio_and_close_window(window_to_close):
     # cannot believe i'm actually making this function
     try:
         pygame.mixer.music.pause()
