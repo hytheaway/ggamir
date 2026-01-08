21edca3dfbba53f39a433d4470ab5c8c58d2802f
hytheaway <91982175+hytheaway@users.noreply.github.com>
2026-01-08 14:27:36 -0500

    Replace dummy funcs, update imports

--- gui/initcontent.py
+++ gui/initcontent.py
@@ -3,8 +3,11 @@
 import shared.globals
 
 import gui.lookandfeel
+import gui.dyncontent
 
+import func.app
 import func.audio
+import func.graphs
 
 import tkinter as tk
 from tkinter import (ttk,)
@@ -27,14 +30,26 @@ def populate_root(root):
     button_frame = tk.Frame(root)
     button_frame.grid(row=1, column=0, columnspan=3)
     
-    shared.globals.file_data_button = ttk.Button(button_frame, text="File data...", style="my.TButton", command=lambda:gui.dyncontent.create_file_data_window(root, shared.globals.audio_file_path), state=shared.globals.graph_buttons_state)
+    shared.globals.file_data_button = ttk.Button(button_frame, text="File data", style="my.TButton", command=lambda:gui.dyncontent.create_file_data_window(root, shared.globals.audio_file_path), state=shared.globals.graph_buttons_state)
     shared.globals.file_data_button.grid(row=0, column=0)
     
-    shared.globals.td_vis_button = ttk.Button(button_frame, text="Time Domain Graph", style="my.TButton", command=lambda:print("time domain vis"), state=shared.globals.graph_buttons_state)
+    shared.globals.td_vis_button = ttk.Button(button_frame, text="Time Domain Graph", style="my.TButton", command=lambda:func.graphs.time_domain_graph(shared.globals.audio_file_path, shared.globals.IR), state=shared.globals.graph_buttons_state)
     shared.globals.td_vis_button.grid(row=0, column=1)
     
-    shared.globals.fd_vis_button = ttk.Button(button_frame, text="Frequency Domain Graph", style="my.TButton", command=lambda:print("freq domain vis"), state=shared.globals.graph_buttons_state)
+    shared.globals.fd_vis_button = ttk.Button(button_frame, text="Frequency Domain Graph", style="my.TButton", command=lambda:func.graphs.freq_domain_graph(shared.globals.audio_file_path, shared.globals.IR, shared.globals.fs), state=shared.globals.graph_buttons_state)
     shared.globals.fd_vis_button.grid(row=0, column=2)
+    
+    nav_spacer = tk.Label(root, text=' ')
+    nav_spacer.grid(row=2, column=1)
+    
+    contact_button = ttk.Button(root, text='Contact', style='my.TButton', command=lambda:gui.dyncontent.create_contact_window())
+    contact_button.grid(row=3, column=0, sticky='w', padx=15)
+    
+    quit_button = ttk.Button(root, text='Quit', style='my.TButton', command=lambda:func.app.quit_function())
+    quit_button.grid(row=3, column=2, sticky='e', padx=15)
+    
+    spacer_bottom = tk.Label(root, text=' ')
+    spacer_bottom.grid(row=4, column=1)
     
     return
 
